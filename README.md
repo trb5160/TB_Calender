83dde634e2ad0f73da0bf0cefd0f1c4090767dd2
Tom Barber <trb5160@gmail.com>
2021-03-16 03:05:54 -0400

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# TB_Calender
+Daily Planner
+
+Description:  As a busy person, it would be very useful to have a schedule I can look at so I know
+what I have going on each day.  To do this, I created a simple daily planner app using html, css and javascript.  The app has timeslots ranging from 9am to 5pm, the typical work day.  I can enter
+stuff into the columns and then use the save button on the side to keep the data there.  
+
+
+Website Link: https://trb5160.github.io/TB_Day_Planner/
+GitHub Link: https://github.com/trb5160/TB_Day_Planner.git
+
+License:
+
+Copyright (c) [2021] [Tom Barber]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,14 +1,42 @@
-//Show current date at top of page
-
+// display current day on page
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-//Display data stored in each timeblock
-var previouslySaved = JSON.parse(localStorage.getItem("timeblocks"));
-
-//Saves any changes made to local storage
-$(".saveBtn").on("click", function() {
-    var data = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, value);
+//function to save data in timeblocks
+$(document).ready(function() {
+  $(".saveBtn").on("click", function() {
+   let value = $(this).siblings(".description").val();
+   let time = $(this).parent().attr("id");
+   localStorage.setItem(time, value);
   });
 
+// load any saved data from localStorage
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+//code for color-changing
+function hourUpdater() {
+    let currentHour = moment().hours();
+    $(".time-block").each(function() {
+      let blockHour = parseInt($(this).attr("id").split("-")[1]);
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } 
+      else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } 
+      else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+});
